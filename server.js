3f66dc74737997f3f1c17e25fcd9edb0fa9d21c0
Emilio Castro <hello@emicastro.com>
2019-12-05 21:14:01 -0300

    Adding router for handle HTTP verbs in requests

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const router = express.Router()
 
 var app = express()
 
-app.use('/', (req, res) => res.send('Hello Word!'))
+app.use(router)
+
+router.get('/', (req, res) => res.send('Get petition'))
+
+router.post('/', (req, res) => res.send('Post petition'))
+
+router.put('/', (req, res) => res.send('Put petition'))
+
+router.delete('/', (req, res) => res.send('Delete petition'))
+
+router.patch('/', (req, res) => res.send('Patch petition'))
 
 app.listen(3000)
-console.log('Server running on port 3000');
+console.log('Server running on http://localhost:3000')
