33142b937fe09ae98bd27af92adb7fc93252b5b4
Emilio Castro <hello@emicastro.com>
2019-12-15 21:52:42 -0300

    Adding method to edit a message

--- components/message/controller.js
+++ components/message/controller.js
@@ -25,7 +25,20 @@ function getMessages() {
     })
 }
 
+function updateMessage(id, message) {
+    return new Promise(async (resolve, reject) => {
+        if(!id || !message) {
+            reject('Invalid data')
+            return false;
+        }
+
+        const result = await store.update(id, message)
+        resolve(result)
+    })
+}
+
 module.exports = {
     addMessage,
     getMessages,
+    updateMessage,
 }
--- components/message/store.js
+++ components/message/store.js
@@ -13,14 +13,24 @@ function addMessage(message) {
     myMessage.save()
 }
 
-async function getMessage() {
+async function getMessages() {
     const messages = await Model.find()
     return messages
 }
 
+async function updateText(id, message) {
+    const foundMessage = await Model.findOne({
+        _id: id
+    })
+    foundMessage.message = message
+    const newMessage = await foundMessage.save()
+    return newMessage
+}
+
 module.exports = {
     add: addMessage,
-    list: getMessage,
+    list: getMessages,
+    update : updateText,
     // get
     // update
     // delete
