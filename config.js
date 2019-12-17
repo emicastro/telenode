3061135c74473672700e1826f8a9fbc45c057fea
Emilio Castro <hello@emicastro.com>
2019-12-17 17:16:48 -0300

    Security fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://db_user:BhJi8ywvZrT7YQc5@cluster0-lxzrb.mongodb.net/telegrom_db',
+    dbUrl: process.env.DB_URL,
     port: process.env.PORT || 3000,
     host: process.env.HOST || 'http://localhost',
     publicPath: process.env.PUBLIC_PATH || '/app',
